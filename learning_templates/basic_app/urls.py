7807b954e846eb2b8a59ac60fd98b98e92abb1b4
DiogoBerti <diogoberti88@gmail.com>
2018-01-10 22:14:28 -0200

    [ADD] - Formularios de registro para novos usuarios

--- learning_templates/basic_app/urls.py
+++ learning_templates/basic_app/urls.py
@@ -9,4 +9,5 @@ app_name = 'basic_app'
 urlpatterns = [
     url(r'^relative/$', views.relative,name='relative'),
     url(r'^other/$', views.other, name='other'),
+    url(r'^register/$', views.register, name='register'),
 ]
